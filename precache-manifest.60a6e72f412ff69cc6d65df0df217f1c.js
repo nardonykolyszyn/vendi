d412323136c07c18fc881e8186117bb7339c1b23
devpolish <nardonykolyszyn@gmail.com>
2020-02-04 11:58:23 -0500

    Updates

---
+++ precache-manifest.60a6e72f412ff69cc6d65df0df217f1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a7c606caa15b368c314997c3ef6231",
+    "url": "/vendi/index.html"
+  },
+  {
+    "revision": "6abb26314131288ac74e",
+    "url": "/vendi/static/css/main.5dccbc96.chunk.css"
+  },
+  {
+    "revision": "31bbde38508d018ca101",
+    "url": "/vendi/static/js/2.05efd164.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vendi/static/js/2.05efd164.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6abb26314131288ac74e",
+    "url": "/vendi/static/js/main.e53d8305.chunk.js"
+  },
+  {
+    "revision": "36efed335a6772269802",
+    "url": "/vendi/static/js/runtime-main.1c7dc627.js"
+  },
+  {
+    "revision": "7b2bfb6d68c8b5ad474a6b2a39342106",
+    "url": "/vendi/static/media/2181fcf3c99d446388410d45ebe1c2f94cfa.7b2bfb6d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vendi/static/media/logo.5d5d9eef.svg"
+  }
+]);
